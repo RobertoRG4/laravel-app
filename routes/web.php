b40352ed9dd8ef87cb4eb09fa5b52480fe064d29
RobertoRG4 <robertoromeroguadarrama@gmail.com>
2025-04-01 19:52:13 -0600

    login dashboard user and password default

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
-use App\Http\Controllers\GenderController;
-use App\Http\Controllers\SuperHerosController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Universo;
-use App\Http\Controllers\UniverseController;
 
 Route::get('/', function () {
-    echo 'Hello guys this is my first Laravel project';
-    /*echo'<prev>';
-    print_r(Universe::all());
-    echo '</prev>';*/
-    dump(Universo::all());
+    return view('welcome');
 });
 
-//Route::get("/universes", [UniverseController::class, "index"]);
-//Route::post("/universes/create", [UniverseController::class,"create"]);
-Route::resource("universes", UniverseController::class);
-Route::resource("/gender", GenderController::class);
-Route::resource("superheros", SuperHerosController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
