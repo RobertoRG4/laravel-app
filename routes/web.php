113cd9b2a020435d9d7ea28662bec55c7269fed1
RobertoRG4 <robertoromeroguadarrama@gmail.com>
2025-04-11 21:52:46 -0600

    api

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/api/universes",[UniverseApiController::class, "index"]);
-Route::get("/api/universes/{name}",[UniverseApiController::class, "show"]);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
